5cc4d3b538aa9c37c4b9441abde0baf1b456196b
Aida Bozulan <49568146+aidskiy@users.noreply.github.com>
2024-08-16 14:25:49 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import "@fontsource/outfit"
 import "@fontsource/roboto"
@@ -10,6 +11,8 @@ import "@fontsource/roboto"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>
   </React.StrictMode>
 );
