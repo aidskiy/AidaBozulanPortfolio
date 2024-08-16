9205204df4f82398ce6fdcd76db90e63607345a2
Aida Bozulan <49568146+aidskiy@users.noreply.github.com>
2024-08-16 15:36:02 -0700

    didn't work

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter as Router } from "react-router-dom";
 
 import "@fontsource/outfit"
 import "@fontsource/roboto"
@@ -11,8 +10,6 @@ import "@fontsource/roboto"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>
 );
